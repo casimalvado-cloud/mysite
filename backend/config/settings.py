5c9f0f8fb8db76970b75b012703acf057985ebbd
Christopher <casimalvado@gmail.com>
2026-06-04 16:28:54 -0300

    date base actualy

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -52,8 +52,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysite',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
